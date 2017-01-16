39079397fe72bdded32c429b10060d8fa50f5601
Saturn91 <manuel.geissberger@gmail.com>
2017-01-16 11:11:11 +0100

    [INIT]

---
+++ PerlinNoiseMapGeneration/src/com/saturn91/perlinNoise/PerlinNoise.java
@@ -0,0 +1,153 @@
+package com.saturn91.perlinNoise;
+
+import java.util.Random;
+
+/**
+ * This Class is based on the Internetside:
+ * http://devmag.org.za/2009/04/25/perlin-noise/
+ * @author M.Geissbberger
+ *
+ */
+public class PerlinNoise {
+	private float[][] smoothNoiseOutput;
+	
+	private int width;
+	private int height;
+	
+	public PerlinNoise(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	/**
+	 * This Methode generates a float[width][height] with values 0 and 1
+	 * @param width -> of map
+	 * @param height -> of map
+	 * @param seed -> to get same results again
+	 * @return a white noise value 0 or 1 on each coordinate
+	 */
+	public float[][] generateWhiteNoise(long seed){
+		Random random = new Random(seed); //Seed to 0 for testing
+		float[][] noise = new float[width][height];
+
+		for (int x = 0; x < width; x++){
+			for (int y = 0; y < height; y++){
+				noise[x][y] = (float)random.nextDouble() % 1;
+			}
+		}
+
+		return noise;
+	}
+	
+	/**
+	 * 
+	 * @param baseNoise
+	 * @param octave
+	 * @return
+	 */
+	public float[][] GenerateSmoothNoise(float[][] baseNoise, int octave){
+		
+		smoothNoiseOutput = new float[width][height];
+		
+		
+		int samplePeriod = (int) Math.pow(2, octave); // calculates 2 ^ octave
+		float sampleFrequency = 1.0f / samplePeriod;
+
+		for (int x = 0; x < width; x++)
+		{
+			//calculate the horizontal sampling indices
+			int sample_x0 = (x / samplePeriod) * samplePeriod;
+			int sample_x1 = (sample_x0 + samplePeriod) % width; //wrap around
+			float horizontal_blend = (x - sample_x0) * sampleFrequency;
+
+			for (int y = 0; y < height; y++)
+			{
+				//calculate the vertical sampling indices
+				int sample_j0 = (y / samplePeriod) * samplePeriod;
+				int sample_j1 = (sample_j0 + samplePeriod) % height; //wrap around
+				float vertical_blend = (y - sample_j0) * sampleFrequency;
+
+				//blend the top two corners
+				float top = Interpolate(baseNoise[sample_x0][sample_j0],
+						baseNoise[sample_x1][sample_j0], horizontal_blend);
+
+				//blend the bottom two corners
+				float bottom = Interpolate(baseNoise[sample_x0][sample_j1],
+						baseNoise[sample_x1][sample_j1], horizontal_blend);
+
+				//final blend
+				try {
+					smoothNoiseOutput[x][y] = Interpolate(top, bottom, vertical_blend);
+				} catch (Exception e) {
+					System.out.println("x:" + x + "y:" + y + ":" + top + ":" + bottom + ":" + vertical_blend);
+					System.exit(0);
+				}
+
+			}
+		}
+
+		return smoothNoiseOutput;
+	}
+
+	/**
+	 * Linear Interpolation between value x0 and x1
+	 * @param x0
+	 * @param x1
+	 * @param alpha
+	 * @return
+	 */	
+	private float Interpolate(float x0, float x1, float alpha)
+	{
+		return x0 * (1 - alpha) + alpha * x1;
+	}
+
+
+	/**
+	 * Pernal Noise Generator
+	 * @param baseNoise
+	 * @param octaveCount
+	 * @return
+	 */
+	public float[][] GeneratePerlinNoise(float[][] baseNoise, int octaveCount)
+	{
+
+		float[][][] smoothNoise = new float[octaveCount][][]; //an array of 2D arrays containing
+
+		float persistance = 0.5f;
+
+		//generate smooth noise
+		for (int i = 0; i < octaveCount; i++)
+		{
+			smoothNoise[i] = GenerateSmoothNoise(baseNoise, i);
+		}
+
+		float[][] perlinNoise = new float[width][height];
+		float amplitude = 1.0f;
+		float totalAmplitude = 0.0f;
+
+		//blend noise together
+		for (int octave = octaveCount - 1; octave >= 0; octave--)
+		{
+			amplitude *= persistance;
+			totalAmplitude += amplitude;
+
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					perlinNoise[x][y] += smoothNoise[octave][x][y] * amplitude;
+				}
+			}
+		}
+
+		//normalisation
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				perlinNoise[x][y] /= totalAmplitude;
+			}
+		}
+
+		return perlinNoise;
+	}
+}
---
+++ PerlinNoiseMapGeneration/src/com/saturn91/perlinNoise/main/Launcher.java
@@ -0,0 +1,60 @@
+package com.saturn91.perlinNoise.main;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.saturn91.perlinNoise.PerlinNoise;
+
+public class Launcher {
+	private static float waterhight = 0.45f;
+	
+	public static void main(String[] args){
+		int width = 1000;
+		int height = 1000;
+		generateMap(width, height, 12345);		
+	}
+	
+	private static void generateMap(int width, int height, long seed){
+		long startTime = System.currentTimeMillis();
+		PerlinNoise noise = new PerlinNoise(width, height);
+		float[][] baseNoise = noise.generateWhiteNoise(seed);
+		float[][] perlinNoise = noise.GeneratePerlinNoise(baseNoise, 7);
+		
+		System.out.println("generated Map in: " + ((float) (System.currentTimeMillis()-startTime)/1000) + "s");
+		startTime = System.currentTimeMillis();
+		BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+		
+		int procent = 0;
+		for(int x = 0; x < width; x++){
+			procent = (int) ((float)x/ (float) width*100);
+			System.out.println("loaded: " + procent + "%");
+			for(int y = 0; y < height; y++){
+				int r = (int) (perlinNoise[x][y]*255.0f);
+				int g = (int) (perlinNoise[x][y]*255.0f);
+				int b = (int) (perlinNoise[x][y]*255.0f);
+				if(perlinNoise[x][y] >= waterhight){
+					r = 0;
+					b = 0;
+				}else{
+					r = 0;
+					g = 0;
+				}
+				
+				int col = (r << 16) | (g << 8) | b;
+				bufferedImage.setRGB(x, y, col);
+			}
+		}
+		
+		try {
+		    File outputfile = new File("map" +  seed + "_" + waterhight + ".png");
+		    ImageIO.write(bufferedImage, "png", outputfile);
+		    System.out.println("painted Map in: " + ((float) (System.currentTimeMillis()-startTime)/1000) + "s");
+		    System.out.println("savedMap"+ seed);
+		} catch (IOException e) {
+		    System.err.println("Failed to paint map!");
+		}		
+	}
+}
